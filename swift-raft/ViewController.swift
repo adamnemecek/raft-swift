49ba239a2810189df0c52330fbb1acdd6542b08f
Franky Liang <fliangz96@gmail.com>
2017-06-28 16:34:49 -0400

    Initial Commit

---
+++ swift-raft/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-raft
+//
+//  Created by Frank the Tank on 6/28/17.
+//  Copyright © 2017 Frank the Tank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
