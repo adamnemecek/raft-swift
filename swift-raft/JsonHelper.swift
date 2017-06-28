0e8f8acc4e83e5db53556dbdf82175bcd54e97f8
Franky Liang <fliangz96@gmail.com>
2017-06-28 17:35:57 -0400

    Create json helper class to handle everything related to json

---
+++ swift-raft/JsonHelper.swift
@@ -0,0 +1,98 @@
+//
+//  JsonHelper.swift
+//  swift-raft
+//
+//  Created by Frank the Tank on 6/28/17.
+//  Copyright © 2017 Frank the Tank. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class JsonHelper {
+    enum JsonError: Error {
+        case couldNotConvertJsonToData
+    }
+    
+    static func createLogEntryJson(_ message: String, term: Int, leaderIp: String) -> JSON {
+        let logEntryJson: JSON = [
+            "type": "entry",
+            "message": message,
+            "term": term,
+            "leaderIp": leaderIp
+        ]
+        
+        return logEntryJson
+    }
+    
+    static func createRedirectMessageJson(_ message: String) -> JSON {
+        // Potentially need a term?
+        let redirectJson: JSON = [
+            "type": "redirect",
+            "message": message,
+        ]
+        
+        return redirectJson
+    }
+    
+    static func createRequestVoteRequestJson(_ candidateTerm: Int, lastLogTerm: Int, lastLogIndex: Int, sender: String) -> JSON {
+        let requestJson: JSON = [
+            "type" : "requestVoteRequest",
+            "candidateTerm" : candidateTerm,
+            "lastLogTerm" : lastLogTerm,
+            "lastLogIndex" : lastLogIndex,
+            "sender" : sender
+        ]
+        
+        return requestJson
+    }
+    
+    static func createRequestVoteResponseJson(_ term: Int, granted: Bool, sender: String) -> JSON {
+        let responseJson: JSON = [
+            "type": "requestVoteResponse",
+            "term": term,
+            "granted": granted,
+            "sender": sender
+        ]
+        
+        return responseJson
+    }
+
+    static func createAppendEntriesRequestJson(_ leaderIp: String, message: String, senderCurrentTerm: Int, prevLogIndex: Int, prevLogTerm: Int, leaderCommitIndex: Int) -> JSON {
+        let requestJson : JSON = [
+            "type" : "appendEntriesRequest",
+            "leaderIp" : leaderIp,
+            "message" : message,
+            "senderCurrentTerm" : senderCurrentTerm,
+            "prevLogIndex" : prevLogIndex,
+            "prevLogTerm" : prevLogTerm,
+            "leaderCommitIndex" : leaderCommitIndex
+        ]
+        
+        return requestJson
+    }
+    
+    static func createAppendEntriesResponseJson(_ success: Bool, senderCurrentTerm: Int, sender: String) -> JSON {
+        let responseJson: JSON = [
+            "type" : "appendEntriesResponse",
+            "success" : success,
+            "senderCurrentTerm" : senderCurrentTerm,
+            "sender" : sender
+        ]
+        
+        return responseJson
+    }
+    
+    static func convertJsonToData(_ json: JSON) throws -> Data {
+        guard let jsonData = json.rawString()?.data(using: String.Encoding.utf8) else {
+            print("Couldn't create JSON")
+            throw JsonError.couldNotConvertJsonToData
+        }
+        
+        return jsonData
+    }
+    
+    static func convertDataToJson(_ data: Data) -> JSON {
+        return JSON(data: data)
+    }
+}
