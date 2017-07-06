077e70f00a35f8a34e6a2fb57ae55ea8fe0d767f
Franky Liang <fliangz96@gmail.com>
2017-07-05 21:05:43 -0400

    Move socket into RpcManager

---
+++ swift-raft/NextIndex.swift
@@ -0,0 +1,13 @@
+//
+//  NextIndex.swift
+//  swift-raft
+//
+//  Created by Frank the Tank on 7/5/17.
+//  Copyright © 2017 Frank the Tank. All rights reserved.
+//
+
+import Foundation
+
+class NextIndex {
+
+}
--- swift-raft/RpcManager.swift
+++ swift-raft/RpcManager.swift
@@ -8,8 +8,10 @@
 
 import Foundation
 import SwiftyJSON
+import CocoaAsyncSocket
 
 class RpcManager {
+    // MARK: RPC Manager Variables
     var currentTerm: Int // server manager?
     var cluster: Cluster?
     var socket: Socket?
@@ -22,34 +24,95 @@ class RpcManager {
         case Leader
     }
     
+    // MARK: Socket Variables
+    var udpUnicastSocket: GCDAsyncUdpSocket?
+    
+    // MARK: Initialize RpcManager Singleton
+    
     private init() {
+        // Initialize RpcManager variables
         cluster = Cluster()
-        socket = Socket()
         log = Log()
         currentTerm = 1
+        
+        // Initialize Socket variables
+        let unicastQueue = DispatchQueue.init(label: "unicast")
+        udpUnicastSocket = GCDAsyncUdpSocket(delegate: self as? GCDAsyncUdpSocketDelegate, delegateQueue: unicastQueue)
+        setupUnicastSocket()
     }
     
     static let shared = RpcManager()
+    
+    // MARK: Socket Methods
+    
+    func setupUnicastSocket() {
+        guard let socket = udpUnicastSocket else {
+            print("Couldn't setup sockets")
+            return
+        }
+        
+        do {
+            try socket.bind(toPort: 20011)
+            try socket.beginReceiving()
+        } catch {
+            print(error)
+        }
+    }
+    
+    func sendJsonUnicast(jsonToSend: Data, targetHost: String) {
+        guard let socket = udpUnicastSocket else {
+            print("Socket or leaderIp could not be initialized")
+            return
+        }
+        
+        socket.send(jsonToSend, toHost: targetHost, port: 20011, withTimeout: -1, tag: 0)
+    }
+    
+    // Receive UDP packets
+    func udpSocket(_ sock: GCDAsyncUdpSocket, didReceive data: Data, fromAddress address: Data, withFilterContext filterContext: Any?) {
+        let receivedJSON = JsonHelper.convertDataToJson(data)
+        let jsonReader = JsonReader(receivedJSON)
+        
+        if (jsonReader.type == "redirect") {
+            // Handle redirecting message to leader
+            guard let message = jsonReader.message else {
+                print("Couldn't get message for redirect")
+                return
+            }
+            receiveClientMessage(message)
+        } else if (jsonReader.type == "appendEntriesRequest") {
+            // Handle append entries request
+            //            handleAppendEntriesRequest(receivedJSON: receivedJSON)
+        } else if (jsonReader.type == "appendEntriesResponse") {
+            // Handle success and failure
+            // Need to check if nextIndex is still less, otherwise send another appendEntries thing
+            //            handleAppendEntriesResponse(receivedJSON: receivedJSON)
+        } else if (jsonReader.type == "requestVoteRequest") {
+            //            handleRequestVoteRequest(receivedJSON: receivedJSON)
+        } else if (jsonReader.type == "requestVoteResponse") {
+            //            handleRequestVoteResponse(receivedJSON: receivedJSON)
+        }
+    }
+    
+    // MARK: Handle RPC Methods
     
     func receiveClientMessage(_ message: String) {
         guard let leaderIp = cluster?.leaderIp else {
             print("No leader IP")
             return
         }
-        print("Received a client message")
         
-        if (role == Role.Follower || role == Role.Candidate || leaderIp != cluster?.selfIp) {
-            // Redirect request to leader
-            if let jsonToSend = JsonHelper.convertJsonToData(JsonHelper.createRedirectMessageJson(message)) {
-                socket?.sendJsonUnicast(jsonToSend: jsonToSend, targetHost: leaderIp)
-            }
-        } else if (role == Role.Leader) {
+        if (role == Role.Leader) {
             // Add to log and send append entries RPC
             let jsonToStore = JsonHelper.createLogEntryJson(message: message, term: currentTerm, leaderIp: leaderIp)
             log?.addEntryToLog(jsonToStore)
             updateLogTextField()
             appendEntries()
-            print("Called appendEntries")
+        } else {
+            // Redirect request to leader
+            if let jsonToSend = JsonHelper.convertJsonToData(JsonHelper.createRedirectMessageJson(message)) {
+                socket?.sendJsonUnicast(jsonToSend: jsonToSend, targetHost: leaderIp)
+            }
         }
     }
     
